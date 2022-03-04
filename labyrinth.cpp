fd434eb8b7d2fa4a7b01de590e5d74e725c59c4c
tanflem <tanner.fleming@gmail.com>
2022-03-04 08:39:16 -0500

    Got it to actually work now

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <unistd.h>
+#include <random>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
+#include <functional>
+#include <chrono>
 
-#define NUMGUESTS 10
-
-int num_guests = NUMGUESTS;
+#define NUMGUESTS 100
 
 // Mutex to allow only one guest in at a time
 std::mutex in_labyrinth;
@@ -13,16 +16,16 @@ std::mutex in_labyrinth;
 // Condition variable to let the Minotaur know to find another guest
 std::condition_variable exit_labyrinth;
 
-void guest(int id) {
-    // Variable to track the first guest that will eat the cupcakes
-    int eater = 0;
+// Variable to track the first guests id and Flag to find the first guest
+std::atomic<int> eater;
+std::atomic<bool> first_flag = true;
 
-    // bool to track if the cupcake has been refreshed or not
-    bool cupcake = true;
+// bool to track if the cupcake has been refreshed or not
+std::atomic<bool> cupcake = true;
 
-    // Flag to find the first guest
-    bool first_flag = true;
+std::atomic<int> num_guests(NUMGUESTS);
 
+void guest(int chosen) {
     // Array of bools to track if any threads have been through the labyrinth
     bool been_there[NUMGUESTS];
 
@@ -31,61 +34,70 @@ void guest(int id) {
         been_there[i] = false;
     }
 
+    // Minotaur random selection
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(0, NUMGUESTS);
+    auto minotaur = std::bind ( distribution, generator);
+
     // While their are guests who have not entered the labyrinth
     while (num_guests > 0){
+        // Lock to keep more than one guest from getting into the maze
         std::unique_lock<std::mutex> labyrinth_lock(in_labyrinth);
-        while((id != num_guests % NUMGUESTS) && (num_guests > 0)){
+        
+        while((chosen == minotaur()) && (num_guests > 0)){
             // Exploring the castle and waiting for the minotaur to call them in
-            exit_labyrinth.wait(labyrinth_lock); // Someone in Labrynith already
+            exit_labyrinth.wait(labyrinth_lock);
         }
+
         if( num_guests > 0){
 
             if(first_flag){
                 // First person in becomes the eater
-                eater = id;
+                eater = chosen;
                 // Set to false after the first person
                 first_flag = false; 
             }
 
             // If the person is the eater
-            if(id == eater){
+            if(chosen == eater){
                 // And there is a cupcake present
                 if(cupcake){
                     // Eat the cupcake
                     cupcake = false;
+                    
                     // And decrement the number of guests left to enter
                     num_guests--;
                 }
             }
             // Else the person is not the eater
-            else{
-                // If it is their first time entering the maze refresh cupcake
-                if(been_there[id]){
-                    been_there[id] = true;
-                    if(!cupcake){
+            else{               
+                // If the cupcake is not refreshed and 
+                if(!cupcake){
+                    // And it is their first time in the maze. 
+                    if(been_there[chosen] == false){
+                        // Remember fro yourself that you have been there
+                        been_there[chosen] = true;
+                        // Refresh cupcake
                         cupcake = true;
                     }
                 }
-                
-                // Else do nothing
-                else{
-
-                }
             }
-            printf("Person %d went through the labyrinth.\n", id);
-
-            labyrinth_lock.unlock(); // Unlock the labryrinth
-            exit_labyrinth.notify_all(); // Minotaur tells another guest to enter labyrinth
+            // Unlock the labyrinth and let the minotaur notify another guest
+            labyrinth_lock.unlock(); 
+            exit_labyrinth.notify_all();
         }
     }
 }
 
 int main(){
 
-    // Minotaur invites 5 guests
+    clock_t start, end;
+
+
+    start = clock();
     std::thread guestThreads[NUMGUESTS];
-    
-    for (int i = 0; i < NUMGUESTS; i++) {
+
+    for(int i = 0; i < NUMGUESTS; i++){
         guestThreads[i] = std::thread(guest, i);
     }
 
@@ -93,4 +105,10 @@ int main(){
         gt.join();
     }
 
+    end = clock();
+
+    float time = ((float) end - start) / CLOCKS_PER_SEC;
+
+    std::cout << "ALL GUESTS HAVE COMPLETED THE LABYRINTH IN " << time << " seconds\n";
 }
+
